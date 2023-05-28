b1b64e1322b4bafce596cb75835a58bb5ec9b0e2
j-a-marin <j.a.marin@icloud.com>
2023-05-28 15:46:43 -0700

    neo4j query results iterable to python dict (v2)

--- helpers_viz.py
+++ helpers_viz.py
@@ -1,23 +1,11 @@
-def visualize_object(driver, results):
+data = [{
+    "nodes": [node._properties for node in record["path"].nodes],
+    "relationships": [relationship._properties for relationship in record["path"].relationships]
+} for record in result]
+
+def visualize_object(driver, data):
     graph = {
-        "nodes": [
-            {
-                "account_id": result["account_id"],
-                "label": "Application",
-                "name_dob": result["name_dob"],
-                "business_name_legal": result["business_name_legal"],
-                "business_name_dba": result["business_name_dba"],
-                "business_address": result["business_address"],
-                "business_phone": result["business_phone"],
-                "mobile_phone": result["mobile_phone"],
-                "ein": result["ein"],
-                "ssn": result["ssn"],
-                "email_address": result["email_address"],
-                "address": result["address"],
-                "ip_address": result["ip_address"]
-            }
-            for result in results
-        ],
+        "nodes": [],
         "relationships": []
     }
 
@@ -26,13 +14,30 @@ def visualize_object(driver, results):
             'mobile_phone', 'ein', 'ssn', 'email_address', 'address', 'ip_address',
             'fraud_flag']
 
-    for key in keys:
-        for result in results:
-            for relationship in result[key]:  # Assuming result[key] is a list of relationship dictionaries
-                graph["relationships"].append({
-                    "startNode": result["account_id"],
-                    "endNode": relationship["account_id"],  # Assuming relationship is a dictionary with an "account_id" key
-                    "type": key.upper()
-                })
+    for record in data:
+        for node in record["nodes"]:
+            graph["nodes"].append({
+                "account_id": node.get("account_id"),
+                "label": "Application",
+                "name_dob": node.get("name_dob"),
+                "business_name_legal": node.get("business_name_legal"),
+                "business_name_dba": node.get("business_name_dba"),
+                "business_address": node.get("business_address"),
+                "business_phone": node.get("business_phone"),
+                "mobile_phone": node.get("mobile_phone"),
+                "ein": node.get("ein"),
+                "ssn": node.get("ssn"),
+                "email_address": node.get("email_address"),
+                "address": node.get("address"),
+                "ip_address": node.get("ip_address")
+            })
+        for relationship in record["relationships"]:
+            for key in keys:
+                if key in relationship:
+                    graph["relationships"].append({
+                        "startNode": relationship.get("startNode"),
+                        "endNode": relationship.get("endNode"),
+                        "type": key.upper()
+                    })
 
     return graph
