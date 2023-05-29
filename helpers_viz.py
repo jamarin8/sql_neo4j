b890abc7e01945c274df03ef1e999c4bb5e0e6db
j-a-marin <j.a.marin@icloud.com>
2023-05-29 09:44:44 -0700

    home_viz revised due to repetition of nodes

--- helpers_viz.py
+++ helpers_viz.py
@@ -42,6 +42,49 @@ def visualize_object(driver, data):
 
     return graph
 
+def visualize_object_2(driver, data):
+    graph = {
+        "nodes": [],
+        "relationships": []
+    }
+
+    keys = ['name_dob', 'account_id', 'business_name_legal',
+            'business_name_dba', 'business_address', 'business_phone',
+            'mobile_phone', 'ein', 'ssn', 'email_address', 'address', 'ip_address',
+            'fraud_flag']
+
+    added_nodes = set()
+
+    for record in data:
+        for node in record["nodes"]:
+            account_id = node.get("account_id")
+            if account_id not in added_nodes:
+                added_nodes.add(account_id)
+                graph["nodes"].append({
+                    "account_id": account_id,
+                    "label": "Application",
+                    "name_dob": node.get("name_dob"),
+                    "business_name_legal": node.get("business_name_legal"),
+                    "business_name_dba": node.get("business_name_dba"),
+                    "business_address": node.get("business_address"),
+                    "business_phone": node.get("business_phone"),
+                    "mobile_phone": node.get("mobile_phone"),
+                    "ein": node.get("ein"),
+                    "ssn": node.get("ssn"),
+                    "email_address": node.get("email_address"),
+                    "address": node.get("address"),
+                    "ip_address": node.get("ip_address")
+                })
+        for relationship in record["relationships"]:
+            graph["relationships"].append({
+                "startNode": relationship.get("startNode"),
+                "endNode": relationship.get("endNode"),
+                "type": relationship.get("type").upper()  # assuming the relationship type is in the 'type' key
+            })
+
+    return graph
+
+
 def check_types(obj):
     if isinstance(obj, dict):
         for key, value in obj.items():
