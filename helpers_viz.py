705b47e3c06a09afbdb10724f9a8787155e1b409
j-a-marin <j.a.marin@icloud.com>
2023-05-29 10:29:40 -0700

    home_viz revised due to repetition of nodes

--- helpers_viz.py
+++ helpers_viz.py
@@ -53,36 +53,53 @@ def visualize_object_2(driver, data):
             'mobile_phone', 'ein', 'ssn', 'email_address', 'address', 'ip_address',
             'fraud_flag']
 
-    added_nodes = set()
-
     for record in data:
-        for node in record["nodes"]:
+        nodes = record["nodes"]
+        relationships = record["relationships"]
+
+        if len(nodes) < 2:
+            print(f"Warning: Skipping a path with fewer than two nodes: {nodes}")
+            continue
+
+        for node in nodes:
             account_id = node.get("account_id")
-            if account_id not in added_nodes:
-                added_nodes.add(account_id)
-                graph["nodes"].append({
-                    "account_id": account_id,
-                    "label": "Application",
-                    "name_dob": node.get("name_dob"),
-                    "business_name_legal": node.get("business_name_legal"),
-                    "business_name_dba": node.get("business_name_dba"),
-                    "business_address": node.get("business_address"),
-                    "business_phone": node.get("business_phone"),
-                    "mobile_phone": node.get("mobile_phone"),
-                    "ein": node.get("ein"),
-                    "ssn": node.get("ssn"),
-                    "email_address": node.get("email_address"),
-                    "address": node.get("address"),
-                    "ip_address": node.get("ip_address")
-                })
-        for relationship in record["relationships"]:
-            graph["relationships"].append({
-                "startNode": relationship.get("startNode"),
-                "endNode": relationship.get("endNode"),
-                "type": relationship.get("type").upper()  # assuming the relationship type is in the 'type' key
+            if account_id is None:
+                print(f"Warning: Skipping a node without an account ID: {node}")
+                continue
+
+            graph["nodes"].append({
+                "account_id": account_id,
+                "label": "Application",
+                "name_dob": node.get("name_dob"),
+                "business_name_legal": node.get("business_name_legal"),
+                "business_name_dba": node.get("business_name_dba"),
+                "business_address": node.get("business_address"),
+                "business_phone": node.get("business_phone"),
+                "mobile_phone": node.get("mobile_phone"),
+                "ein": node.get("ein"),
+                "ssn": node.get("ssn"),
+                "email_address": node.get("email_address"),
+                "address": node.get("address"),
+                "ip_address": node.get("ip_address")
             })
 
+        for relationship in relationships:
+            # Find the type of the relationship
+            type_of_relationship = [key for key in relationship if key in keys]
+            if not type_of_relationship:
+                print(f"Warning: Skipping a relationship without a corresponding type: {relationship}")
+                continue
+
+            for type_rel in type_of_relationship:
+                graph["relationships"].append({
+                    "startNode": nodes[0].get("account_id"),
+                    "endNode": nodes[1].get("account_id"),
+                    "type": type_rel.upper(),
+                })
+
     return graph
+
+
 
 
 def check_types(obj):
