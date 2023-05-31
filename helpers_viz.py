c0dd45cb700a15f63c8aff599c318dc93eb346e7
jamarin8 <85956983+jamarin8@users.noreply.github.com>
2023-05-30 21:19:35 -0700

    Update helpers_viz.py

--- helpers_viz.py
+++ helpers_viz.py
@@ -1,9 +1,61 @@
-data = [{
-    "nodes": [node._properties for node in record["path"].nodes],
-    "relationships": [relationship._properties for relationship in record["path"].relationships]
-} for record in result]
+import json
+import math
 
-def visualize_object(driver, data):
+def remove_duplicates(dicts):
+    serialized = [json.dumps(d, sort_keys=True) for d in dicts]
+    unique_serialized = set(serialized)
+    unique_dicts = [json.loads(d) for d in unique_serialized]
+    return unique_dicts     
+
+def data_refactor(results_dict):
+
+    data = []
+
+    for result in results_dict:
+        path = result['path']
+        nodes = []
+        relationships = []
+
+        for i in range(0, len(path)):
+            if i % 2 == 0:  # nodes are at even indices
+                nodes.append(path[i])
+            else:  # relationships are at odd indices
+                relationships.append(path[i])
+
+        data.append({
+            "nodes": nodes,
+            "relationships": relationships
+        })
+
+    return data
+
+def data_refactor_2(refactored_results_dict):
+
+    data = refactored_results_dict
+
+    def remove_duplicates(dicts):
+        serialized = [json.dumps(d, sort_keys=True) for d in dicts]
+        unique_serialized = set(serialized)
+        unique_dicts = [json.loads(d) for d in unique_serialized]
+        return unique_dicts
+
+    full = {}
+    nodes = [];  relationships = []
+    for element in data:
+        a = element['nodes']
+        b = element['relationships']
+        for aa in a:
+            nodes.append({k:v for k,v in aa.items()})
+        relationships.append({k:v for k,v in b[0].items()})
+
+
+    full['nodes'] = remove_duplicates(nodes)
+    full['relationships'] = relationships
+
+    return full 
+
+
+def visualize_object(data):
     graph = {
         "nodes": [],
         "relationships": []
@@ -11,13 +63,24 @@ def visualize_object(driver, data):
 
     keys = ['name_dob', 'account_id', 'business_name_legal',
             'business_name_dba', 'business_address', 'business_phone',
-            'mobile_phone', 'ein', 'ssn', 'email_address', 'address', 'ip_address',
-            'fraud_flag']
+            'mobile_phone', 'ein', 'ssn', 'email_address', 'address', 'ip_address']
 
     for record in data:
-        for node in record["nodes"]:
+        nodes = record["nodes"]
+        relationships = record["relationships"]
+
+        if len(nodes) < 2:
+            print(f"Warning: Skipping a path with fewer than two nodes: {nodes}")
+            continue
+
+        for node in nodes:
+            account_id = node.get("account_id")
+            if account_id is None:
+                print(f"Warning: Skipping a node without an account ID: {node}")
+                continue
+
             graph["nodes"].append({
-                "account_id": node.get("account_id"),
+                "account_id": account_id,
                 "label": "Application",
                 "name_dob": node.get("name_dob"),
                 "business_name_legal": node.get("business_name_legal"),
@@ -31,19 +94,28 @@ def visualize_object(driver, data):
                 "address": node.get("address"),
                 "ip_address": node.get("ip_address")
             })
-        for relationship in record["relationships"]:
+
+        for i, relationship in enumerate(relationships):
+
+            if i+1 >= len(nodes):
+                print(f"Warning: Skipping a relationship without a corresponding end node: {relationship}")
+                continue
+
             for key in keys:
                 if key in relationship:
                     graph["relationships"].append({
-                        "startNode": relationship.get("startNode"),
-                        "endNode": relationship.get("endNode"),
+                        "startNode": nodes[i].get("account_id"),
+                        "endNode": nodes[i+1].get("account_id"),
                         "type": key.upper()
                     })
+
+    graph["relationships"] = remove_duplicates(graph["relationships"])
 
     return graph
 
 
-def visualize_object_2(driver, data):
+def visualize_object_2(data):
+
     graph = {
         "nodes": [],
         "relationships": []
@@ -51,16 +123,15 @@ def visualize_object_2(driver, data):
 
     keys = ['name_dob', 'account_id', 'business_name_legal',
             'business_name_dba', 'business_address', 'business_phone',
-            'mobile_phone', 'ein', 'ssn', 'email_address', 'address', 'ip_address',
-            'fraud_flag']
+            'mobile_phone', 'ein', 'ssn', 'email_address', 'address', 'ip_address']
 
     for record in data:
         nodes = record["nodes"]
         relationships = record["relationships"]
 
-        if len(nodes) < 2:
-            print(f"Warning: Skipping a path with fewer than two nodes: {nodes}")
-            continue
+        # if len(nodes) < 2:
+        #     print(f"Warning: Skipping a path with fewer than two nodes: {nodes}")
+        #     continue
 
         for i, node in enumerate(nodes):
             account_id = node.get("account_id")
@@ -85,7 +156,7 @@ def visualize_object_2(driver, data):
             })
 
             # If there's a next node and a relationship, create an edge
-            if i < len(nodes) - 1 and i < len(relationships):
+            if i < len(nodes) - 1 : # and i < len(relationships):
                 for relationship in relationships:
                     for key in relationship:
                         if key in keys:
@@ -94,6 +165,8 @@ def visualize_object_2(driver, data):
                                 "endNode": nodes[i + 1].get("account_id"),
                                 "type": key.upper()
                             })
+
+    graph["relationships"] = remove_duplicates(graph["relationships"])
 
     return graph
 
@@ -116,14 +189,12 @@ def transform_data_to_graph(data):
         for i in range(len(nodes) - 1):
             for relationship in relationships:
                 for key in relationship.keys():
-                    edge = {"startNode": nodes[i]["id"], "endNode": nodes[i + 1]["id"], key: relationship[key]}
+                    edge = {"startNode": nodes[i]["account_id"], "endNode": nodes[i + 1]["account_id"], "type": key.upper()}
                     graph["relationships"].append(edge)
 
+    graph["relationships"] = remove_duplicates(graph["relationships"])
+
     return graph
-
-
-import math
-
 
 def transform_data_to_graph_(data):
     graph = {
@@ -156,28 +227,29 @@ def transform_data_to_graph_(data):
             for relationship in relationships:
                 for key, value in relationship.items():
                     graph["relationships"].append({
-                        "startNode": nodes[i]["id"],
-                        "endNode": nodes[i + 1]["id"],
+                        "startNode": nodes[i]["account_id"],
+                        "endNode": nodes[i + 1]["account_id"],
                         "type": key,
                         "value": value
                     })
 
     return graph
 
-
-
 def check_types(obj):
+
+    out = []
+
     if isinstance(obj, dict):
         for key, value in obj.items():
             print(f"Key: {key}, Type: {type(value)}")
-            check_types(value)
+            out.append(check_types(value))
+
     elif isinstance(obj, list):
         for i, item in enumerate(obj):
             print(f"List Index: {i}, Type: {type(item)}")
-            check_types(item)
+            out.append(check_types(item))
     else:
-        print(f"Type: {type(obj)}")
+        out = f"Type: {type(obj)}"
 
-# Call the function on your graph dictionary
-check_types(graph)
+    return out
 
